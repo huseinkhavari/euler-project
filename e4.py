424f9ad36408dd1c7c3c806c72266581dddad8e2
husein <hkhavari5@gmail.com>
2024-04-09 22:38:40 +0330

    4th

---
+++ e4.py
@@ -0,0 +1,14 @@
+import random
+
+def is_palindrome(num):
+    return str(num) == str(num)[::-1]
+
+max_palindrome = 0
+
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        product = i * j
+        if is_palindrome(product) and product > max_palindrome:
+            max_palindrome = product
+
+print(max_palindrome)
